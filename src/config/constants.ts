46f2eae8f22cd0e18b2444d5bf7c2d480528ad9a
Stanley Lim <slim679975@gmail.com>
2019-09-14 03:43:31 -0400

    :sparkles: feat(test.tsx): Working POC for map coloring and display

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -11,3 +11,10 @@ export const MAP_BOX_TOKEN = `pk.eyJ1Ijoic3BpZGVycGlnODYiLCJhIjoiY2swaXV5amZhMDQ
 export const MAP_BOX_THEME_LIGHT = 'mapbox.light';
 export const MAP_BOX_THEME_DARK = 'mapbox.light';
 export const MAP_BOX_THEME_STREETS = 'mapbox.streets';
+
+// TEST FIELDS
+export const HISTORIC_VOTE = 'HistoricVote';
+export const ETHNICITY = 'Ethnicity';
+export const REP_CHANCE = 'RepublicanChance';
+export const DEM_CHANCE = 'DemocratChance';
+export const MINORITY_REP_CHANCE = 'MinorityRepChance';
