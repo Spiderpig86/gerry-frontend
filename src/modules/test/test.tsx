123c7a437a0ff9aa77edfc76b9cebad2e3c92c39
Stanley Lim <slim679975@gmail.com>
2019-09-13 21:44:29 -0400

    :sparkles: feat(test.tsx): Configured Mapbox, sped up map load speeds

--- src/modules/test/test.tsx
+++ src/modules/test/test.tsx
@@ -1,22 +1,24 @@
-import "./test.scss";
-import * as React from "react";
+import * as React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MAP_BOX_ENDPOINT, MAP_BOX_TOKEN } from '../../config/constants';
+
+import "./test.scss";
 
 export class Test extends React.Component {
 
     render() {
         
-        const position = [51.505, -0.09];
+        const position = [40.3,-96.6];
         return (
             <div className="container-fluid dashboard">
                 <h1>Map Testing</h1>
-                <Map center={position} zoom={13}>
+                <Map center={position} zoom={5} style={{ height: '700px' }}>
                     <TileLayer
-                    url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url={MAP_BOX_ENDPOINT + MAP_BOX_TOKEN}
+                    attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
                     />
                     <Marker position={position}>
-                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
                     </Marker>
                 </Map>
             </div>
