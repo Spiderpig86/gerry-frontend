2db16e0d03fb720b396740bdbd50428a2ba60330
spiderpig86 <slim679975@gmail.com>
2019-11-28 16:03:10 -0500

    :art: refactor(blocitem.tsx): Fixed up display for items for phase 0 results

--- src/modules/mapview/components/PhaseZeroTabPanel/blocitem.tsx
+++ src/modules/mapview/components/PhaseZeroTabPanel/blocitem.tsx
@@ -15,49 +15,23 @@ interface BlocItemProps {
 export class BlocItem extends React.Component<BlocItemProps, {}> {
 
     render() {
-        // return (
-        //     <div className='py-3'>
-        //         <h6>{this.props.party}</h6>
-        //         <Table striped bordered hover responsive>
-        //             <thead>
-        //                 <tr>
-        //                     <th>Voting Bloc Count</th>
-        //                     <th>Party Mean</th>
-        //                     <th>Demographic</th>
-        //                     <th>Demographic Mean</th>
-        //                 </tr>
-        //             </thead>
-        //             <tbody>
-        //                 {this.props.phaseZeroResults &&
-        //                     this.props.phaseZeroResults.map((e: PrecinctBlocSummary, i: number) => {
-        //                         return (
-        //                             <tr key={i}>
-        //                                 <td>{e.votingBlocCount}</td>
-        //                                 <td>{e.meanPartyPercentage.toFixed(2)}%</td>
-        //                                 <td>{EnumNameMapper.getDemographicName(e.demographicType)}</td>
-        //                                 <td>{e.meanDemographicPercentage.toFixed(2)}%</td>
-        //                             </tr>
-        //                         );
-        //                     })}
-        //             </tbody>
-        //         </Table>
-        //     </div>
-        // );
 
         const columns = [{
             Header: 'Voting Bloc Count',
             accessor: 'votingBlocCount'
         }, {
             Header: 'Party Mean',
-            accessor: 'meanPartyPercentage'
+            id: 'partyMean',
+            accessor: (e: PrecinctBlocSummary) => `${e.meanPartyPercentage.toFixed(2)}%`
         }, {
             Header: 'Demographic',
-            accessor: 'demographicType'
+            id: 'demographicType',
+            accessor: (e: PrecinctBlocSummary) => EnumNameMapper.getDemographicName(e.demographicType)
         }, {
             Header: 'Demographic Mean',
-            accessor: 'meanDemographicPercentage'
+            id: 'demographicMean',
+            accessor: (e: PrecinctBlocSummary) => `${e.meanDemographicPercentage.toFixed(2)}%`
         }];
-        console.log()
 
         return (
             <div className='py-3'>
