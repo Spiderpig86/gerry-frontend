da8b0d4e404ff9fe2b15643c49e961a812f77ac9
spiderpig86 <slim679975@gmail.com>
2019-11-28 01:58:50 -0500

    :sparkles: feat(blocitem.tsx): Updated tables to react-tables

--- src/modules/mapview/components/PhaseZeroTabPanel/blocitem.tsx
+++ src/modules/mapview/components/PhaseZeroTabPanel/blocitem.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import ReactTable from 'react-table'
+
 import { PhaseZeroResult, PartyEnum, PrecinctBlocSummary } from '../../../../models';
 import { Table } from 'react-bootstrap';
 import { EnumNameMapper } from '../../../../libs/enum-name';
@@ -11,33 +13,61 @@ interface BlocItemProps {
 export class BlocItem extends React.Component<BlocItemProps, {}> {
 
     render() {
+        // return (
+        //     <div className='py-3'>
+        //         <h6>{this.props.party}</h6>
+        //         <Table striped bordered hover responsive>
+        //             <thead>
+        //                 <tr>
+        //                     <th>Voting Bloc Count</th>
+        //                     <th>Party Mean</th>
+        //                     <th>Demographic</th>
+        //                     <th>Demographic Mean</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody>
+        //                 {this.props.phaseZeroResults &&
+        //                     this.props.phaseZeroResults.map((e: PrecinctBlocSummary, i: number) => {
+        //                         return (
+        //                             <tr key={i}>
+        //                                 <td>{e.votingBlocCount}</td>
+        //                                 <td>{e.meanPartyPercentage.toFixed(2)}%</td>
+        //                                 <td>{EnumNameMapper.getDemographicName(e.demographicType)}</td>
+        //                                 <td>{e.meanDemographicPercentage.toFixed(2)}%</td>
+        //                             </tr>
+        //                         );
+        //                     })}
+        //             </tbody>
+        //         </Table>
+        //     </div>
+        // );
+
+        const columns = [{
+            Header: 'Voting Bloc Count',
+            accessor: 'votingBlocCount'
+        }, {
+            Header: 'Party Mean',
+            accessor: 'meanPartyPercentage'
+        }, {
+            Header: 'Demographic',
+            accessor: 'demographicType'
+        }, {
+            Header: 'Demographic Mean',
+            accessor: 'meanDemographicPercentage'
+        }];
+        console.log()
+
         return (
             <div className='py-3'>
                 <h6>{this.props.party}</h6>
-                <Table striped bordered hover responsive>
-                    <thead>
-                        <tr>
-                            <th>Voting Bloc Count</th>
-                            <th>Party Mean</th>
-                            <th>Demographic</th>
-                            <th>Demographic Mean</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.phaseZeroResults &&
-                            this.props.phaseZeroResults.map((e: PrecinctBlocSummary, i: number) => {
-                                return (
-                                    <tr key={i}>
-                                        <td>{e.votingBlocCount}</td>
-                                        <td>{e.meanPartyPercentage.toFixed(2)}%</td>
-                                        <td>{EnumNameMapper.getDemographicName(e.demographicType)}</td>
-                                        <td>{e.meanDemographicPercentage.toFixed(2)}%</td>
-                                    </tr>
-                                );
-                            })}
-                    </tbody>
-                </Table>
+                <ReactTable
+                    columns={columns}
+                    data={this.props.phaseZeroResults}
+                    defaultPageSize={10}
+                    minRows={0}
+                    showPageSizeOptions={false}
+                />
             </div>
-        );
+        )
     }
 }
