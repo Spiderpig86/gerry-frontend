716cc0ed105f858e80ca602e4578de64771834a7
spiderpig86 <slim679975@gmail.com>
2019-12-02 02:04:28 -0500

    :sparkles: feat(model-mapper.ts): Started working on mapping lib

---
+++ src/libs/mapping/model-mapper.ts
@@ -0,0 +1,17 @@
+import { IDemographics, IVoteData } from '../../models';
+
+export class ModelMapper {
+
+    public toIDemographic(demographic: any): IDemographics {
+        return {
+            
+        }
+    }
+
+    public toIVote(vote: any): IVoteData {
+        return {
+            
+        }
+    }
+
+}
