7827678b6ce72ea711308566f84c83c9456b65f8
spiderpig86 <slim679975@gmail.com>
2019-12-02 15:43:28 -0500

    :sparkles: feat(model-mapper.ts): Added lib for transforming backend enums

--- src/libs/mapping/model-mapper.ts
+++ src/libs/mapping/model-mapper.ts
@@ -4,13 +4,22 @@ export class ModelMapper {
 
     public toIDemographic(demographic: any): IDemographics {
         return {
-            
+            White: demographic.pop_white_nh || 0,
+            AfricanAmerican: demographic.pop_black_nh || 0,
+            Asian: demographic.pop_asian_nh || 0,
+            Hispanic: demographic.pop_hispanic || 0,
+            PacificIslander: demographic.pop_nhpi_nh || 0,
+            NativeAmerican: demographic.pop_amin_nh || 0,
+            Biracial: demographic.pop_2more_nh || 0,
+            Other: demographic.pop_other_nh || 0
         }
     }
 
-    public toIVote(vote: any): IVoteData {
+    public toIVote(votes: any): IVoteData {
         return {
-            
+            democraticVotes: votes.democratic || 0,
+            republicanVotes: votes.republican || 0,
+            otherVotes: votes.other || 0
         }
     }
 
